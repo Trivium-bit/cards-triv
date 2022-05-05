bd238eaf7f1d52bc954c336b1cf80db1adc44e02
Trivium-bit <Trivium.exe@gmail.com>
2022-05-05 20:33:29 +0300

    links styles

--- src/n1-main/components/Header.tsx
+++ src/n1-main/components/Header.tsx
@@ -1,17 +1,15 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import s from "../ui/Header.module.css";
 import { PATH } from './Routings'
-import menu_icon from '../../images/menu_icon.png'
 
 function Header() {
   return (
     <div className="Header">
-      <NavLink to={PATH.LOGIN} className={s.link} >Login</NavLink>
-      <NavLink to={PATH.REGISTER} className={s.link} >Register</NavLink>
-      <NavLink to={PATH.PROFILE} className={s.link} >Profile</NavLink>
-      <NavLink to={PATH.NEW_USER} className={s.link} >NewUser</NavLink>
-      <NavLink to={PATH.PASS_RECOVERY} className={s.link} >Password_Recovery</NavLink>
+      <NavLink to={PATH.LOGIN} className={s.link} ><button className={s.button}>Login</button></NavLink>
+      <NavLink to={PATH.REGISTER} className={s.link} ><button className={s.button}>Register</button></NavLink>
+      <NavLink to={PATH.PROFILE} className={s.link} ><button className={s.button}>Profile</button></NavLink>
+      <NavLink to={PATH.NEW_USER} className={s.link} ><button className={s.button}>NewUser</button></NavLink>
+      <NavLink to={PATH.PASS_RECOVERY} className={s.link} ><button className={s.button}>Password_Recovery</button></NavLink>
     </div>
   );
 }
