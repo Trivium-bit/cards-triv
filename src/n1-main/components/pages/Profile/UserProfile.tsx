5035b2c8c08d7ce7ac9476739a6b464f894c8619
marina <marina.gherman@icloud.com>
2022-06-01 15:07:11 +0200

    add modal form for Profile

--- src/n1-main/components/pages/Profile/UserProfile.tsx
+++ src/n1-main/components/pages/Profile/UserProfile.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from "./UserProfile.module.scss";
-import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {AppStoreType} from "../../../bll/store";
 import {ResponseLoginType} from "../../../dall/login-api";
-import Button from "../../../../Common/Components/Button";
+import EditProfileModal from "../EditProfile/EditProfileModal";
+
 
 const UserProfile = () => {
     const user = useSelector<AppStoreType, ResponseLoginType | undefined>((state) => state.app.user);
+
 
     return (
         <>
@@ -19,11 +20,11 @@ const UserProfile = () => {
                     <div className={s.infoGroup}>
                         <div className={s.name}>{user.name}</div>
                         <div className={s.desc}>Front-end Dev</div>
-                        <Link to="/profile/edit">
-                            <Button
-                                className={s.editBtn}
-                                title={'Edit profile'}/>
-                        </Link>
+                        <EditProfileModal
+                            title={'Personal Information'}
+                            email={user.email}
+                            name={user.name}
+                            avatar={user.avatar ||  "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"}/>
                     </div>
                 </div>
             ) : (<div>
