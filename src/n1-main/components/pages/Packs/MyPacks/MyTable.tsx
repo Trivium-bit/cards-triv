b34c17c4344f1f6c758107841afa68b18beba34e
marina <marina.gherman@icloud.com>
2022-06-02 14:41:52 +0200

    add modals for my pack buttons

--- src/n1-main/components/pages/Packs/MyPacks/MyTable.tsx
+++ src/n1-main/components/pages/Packs/MyPacks/MyTable.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { v4 } from 'uuid';
 import {
-    Box,
-    Paper,
+    Box, FormControl, FormControlLabel, Input, InputLabel, Modal,
+    Paper, Radio, RadioGroup,
     styled,
     Table,
     TableBody, TableCell,
     tableCellClasses,
     TableContainer,
     TableHead,
-    TableRow
+    TableRow,
 } from "@mui/material";
+import Button from "../../../../../Common/Components/Button";
 import s from './MyTable.module.scss'
 
+//types
+type CardPropsType = {
+    _id: string
+    name:string
+    cards: number
+    lastUpdate: string
+    createdBy: string
+}
+
+//mui styles
 const StyledTableCell = styled(TableCell)(({ }) => ({
     [`&.${tableCellClasses.head}`]: {
         backgroundColor: "#ECECF9",
@@ -30,46 +41,178 @@ const StyledTableRow = styled(TableRow)(({ }) => ({
         backgroundColor: "#F8F7FD",
     },
 }));
-const rows = [
-    {_id:v4(), name: "Pack name", cards: 4, lastUpdate: "21.02.21", createdBy: "marina"},
-    {_id:v4(), name: "Pack name 2", cards: 4, lastUpdate: "21.02.21", createdBy: "marina"},
-    {_id:v4(), name: "Pack name 3", cards: 4, lastUpdate: "21.02.21", createdBy: "marina"},
+
+const cards: CardPropsType[] = [
+    {_id:v4(), name: "First pack", cards: 4, lastUpdate: "21.02.21", createdBy: "marina 1"},
+    {_id:v4(), name: "Second pack ", cards: 4, lastUpdate: "21.02.21", createdBy: "marina 2"},
+    {_id:v4(), name: "Pack name 3", cards: 4, lastUpdate: "21.02.21", createdBy: "marina 3"},
 ]
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: '#F9F9FE',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: 2,
+};
 
 const MyTable = () => {
+    const [question, setQuestion] = useState("My question is bla?");
+    const [answer, setAnswer] = useState("My answer is bla bla");
+    const [rowToDelete, setRowToDelete] = useState<CardPropsType | undefined>(undefined);
+    const [openAnswer, setOpenAnswer] = useState<CardPropsType | undefined>(undefined);
+    const [openLearn, setOpenLearn] = useState(false);
+    const [openEdit, setOpenEdit] = useState(false);
+
+    const handleOpenDelete = (card: CardPropsType) => setRowToDelete(card);
+    const handleCloseDelete = () => setRowToDelete(undefined);
+
+    const handleOpenAnswer = (card: CardPropsType) => setOpenAnswer(card);
+    const handleCloseAnswer = () => setOpenAnswer(undefined);
+
+    const handleOpenLearn = () => setOpenLearn(true);
+    const handleCloseLearn = () => {
+        setOpenAnswer(undefined);
+        setOpenLearn(false);
+    };
+
+    const handleOpenEdit = () => setOpenEdit(true);
+    const handleCloseEdit = () => setOpenEdit(false);
+
+
+    const handleChangeQuestion = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuestion(event.target.value);
+    };
+    const handleChangeAnswer = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(event.target.value);
+    };
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Name</StyledTableCell>
-                        <StyledTableCell align="right">Cards</StyledTableCell>
-                        <StyledTableCell align="right">Last Updates</StyledTableCell>
-                        <StyledTableCell align="right">Created By</StyledTableCell>
-                        <StyledTableCell align="center">Actions</StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow key={row.name}>
-                            <StyledTableCell component="th" scope="row">
-                                {row.name}
-                            </StyledTableCell>
-                            <StyledTableCell align="right">{row.cards}</StyledTableCell>
-                            <StyledTableCell align="right">{row.lastUpdate}</StyledTableCell>
-                            <StyledTableCell align="right">{row.createdBy}</StyledTableCell>
-                            <StyledTableCell align="right">
-                                <Box className={s.buttonGroup}>
-                                    <button className={s.delete}>Delete</button>
-                                    <button className={s.main}>Edit</button>
-                                    <button className={s.main}>Learn</button>
-                                </Box>
-                            </StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Box>
+            {/*//my table*/}
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell align="right">Cards</StyledTableCell>
+                            <StyledTableCell align="right">Last Updates</StyledTableCell>
+                            <StyledTableCell align="right">Created By</StyledTableCell>
+                            <StyledTableCell align="center">Actions</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {cards.map((card) => (
+                            <StyledTableRow key={card._id}>
+                                <StyledTableCell component="th" scope="row">
+                                    {card.name}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{card.cards}</StyledTableCell>
+                                <StyledTableCell align="right">{card.lastUpdate}</StyledTableCell>
+                                <StyledTableCell align="right">{card.createdBy}</StyledTableCell>
+                                <StyledTableCell align="right">
+                                    <Box className={s.buttonGroup}>
+                                        <button onClick={() => handleOpenDelete(card)} className={s.delete} >Delete</button>
+                                        <button onClick={handleOpenEdit} className={s.main}>Edit</button>
+                                        <button onClick={() => handleOpenAnswer(card)} className={s.main}>Learn</button>
+                                    </Box>
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            {/*// Delete Button Modal*/}
+            <Modal
+                open={!!rowToDelete}
+                onClose={handleCloseDelete}
+            >
+                <Box sx={modalStyle} className={s.modalBlock }>
+                    <h1 className={s.modalTitle}>Delete Pack</h1>
+                    <Box>
+                        <span className={s.modalText}>Do you really want to remove
+                            <b>{rowToDelete?.name}</b>?
+                            All cards will be excluded from this course
+                        </span>
+                    </Box>
+                    <Box className={s.modalBtnGroup}>
+                        <Button onClick={handleCloseDelete} className={s.btnCancel} title={'Cancel'}/>
+                        <Button className={s.btnSave} title={'Save'}/>
+                    </Box>
+                </Box>
+            </Modal>
+
+            {/*// Show Answer Modal*/}
+            <Modal
+                open={!!openAnswer}
+                onClose={handleCloseAnswer}
+            >
+                <Box sx={modalStyle} className={s.modalBlock }>
+                    <h1 className={s.modalTitle}>{openAnswer?.name}</h1>
+                    <p className={s.modalText}><b>Question:</b>“How "This" works in JavaScript?”</p>
+                    <Box className={s.modalBtnGroup}>
+                        <Button onClick={handleCloseAnswer} className={s.btnCancel} title={'Cancel'}/>
+                        <Button onClick={handleOpenLearn} className={s.btnSave} title={'Show Answer'}/>
+                    </Box>
+                </Box>
+            </Modal>
+
+
+            {/*Learn Modal*/}
+            <Modal
+                open={openLearn}
+                onClose={handleCloseLearn}
+            >
+                <Box sx={modalStyle} className={s.modalBlock }>
+                    <h1 className={s.modalTitle}>Learn {openAnswer?.name}</h1>
+                    <p className={s.modalText}><b>Question:</b>“How "This" works in JavaScript?”</p>
+                    <p className={s.modalText}><b>Answer:</b>“This is how "This" works in JavaScript”</p>
+                    <p className={s.modalText}><b>Rate yourself:</b></p>
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="female"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="Did you know" control={<Radio />} label="Did you know" />
+                        <FormControlLabel value="Forgot" control={<Radio />} label="Forgot" />
+                        <FormControlLabel value="Confused" control={<Radio />} label="Confused" />
+                    </RadioGroup>
+                    <Box className={s.modalBtnGroup}>
+                        <Button onClick={handleCloseLearn} className={s.btnCancel} title={'Cancel'}/>
+                        <Button className={s.btnSave} title={'Next'}/>
+                    </Box>
+                </Box>
+            </Modal>
+
+            {/*Edit Modal*/}
+            <Modal
+                open={openEdit}
+                onClose={handleCloseEdit}
+            >
+                <Box sx={modalStyle} className={s.modalBlock }>
+                    <h1 className={s.modalTitle}>Card Info</h1>
+                    <Box>
+                        <FormControl variant="standard">
+                            <InputLabel htmlFor="component-simple">Question</InputLabel>
+                            <Input  className={s.inputsForm} id="component-simple" value={question} onChange={handleChangeQuestion} />
+                        </FormControl>
+                    </Box>
+                    <Box>
+                        <FormControl variant="standard">
+                            <InputLabel htmlFor="component-simple">Answer</InputLabel>
+                            <Input className={s.inputsForm} id="component-simple" value={answer} onChange={handleChangeAnswer} />
+                        </FormControl>
+                        <Box className={s.modalBtnGroup}>
+                            <Button onClick={handleCloseEdit} className={s.btnCancel} title={'Cancel'}/>
+                            <Button className={s.btnSave} title={'Save'}/>
+                        </Box>
+                    </Box>
+                </Box>
+            </Modal>
+        </Box>
     );
 };
 
