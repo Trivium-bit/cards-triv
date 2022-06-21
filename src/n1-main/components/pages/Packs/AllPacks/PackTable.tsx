659314c9473e44dbb4975aca805644bb57523b86
DmitryLotkov <dmitry.lotkov9@gmail.com>
2022-06-22 00:19:57 +0300

    66. Some logo fixes v1 adn table fixes

--- src/n1-main/components/pages/Packs/AllPacks/PackTable.tsx
+++ src/n1-main/components/pages/Packs/AllPacks/PackTable.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {
-    Box, IconButton, Pagination,
-    Paper, Popover,
+    Box, Pagination,
+    Paper,
     styled,
     Table,
     TableBody, TableCell,
@@ -11,7 +11,7 @@ import {
     TableRow
 } from "@mui/material";
 import s from './AllTable.module.scss'
-import {useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {
     myCardsPacksSelector,
     userIdSelector,
@@ -41,10 +41,6 @@ import {maxCardPackNameLength} from "../PacksHeader";
 import TableSortLabel from "@mui/material/TableSortLabel";
 
 
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import DeleteSweepOutlinedIcon from '@mui/icons-material/DeleteSweepOutlined';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 //styles mui
 const StyledTableCell = styled(TableCell)((theme) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -80,10 +76,10 @@ const StyledTableRow = styled(TableRow)(() => ({
     '&:nth-of-type(odd)': {
         backgroundColor: "#F8F7FD",
     },
-    "&:hover": {
+    /*"&:hover": {
         backgroundColor: "#c8b7f8",
         cursor: "pointer"
-    }
+    }*/
 }));
 export const modalStyle = {
     position: 'absolute' as 'absolute',
@@ -116,8 +112,6 @@ export const PackTable = React.memo(() => {
     const [rowToDelete, setRowToDelete] = useState<PacksResponseType | undefined>(undefined);
     const [rowToUpdate, setRowToUpdate] = useState<CardPackUpdateRequestType | undefined>(undefined);
     const [rowToLearn, setRowToLearn] = useState<PacksResponseType | undefined>(undefined);
-    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-    const open = Boolean(anchorEl);
 
     const handleOpenEdit = (cardPack: CardPackUpdateRequestType | undefined) => setRowToUpdate(cardPack);
     const handleOpenDelete = (cardPack: PacksResponseType | undefined) => setRowToDelete(cardPack);
@@ -126,22 +120,15 @@ export const PackTable = React.memo(() => {
         dispatch(setCardPackCurrentPageAC(page))
         dispatch(getCardsPacksTC())
     }
-    const navigateToCardsPage = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>, cardPack: PacksResponseType) => navigate(`${PATH.PACKS}/${cardPack._id}`)
+    const navigateToCardsPage = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>, cardPack: PacksResponseType) => {
+        navigate(`${PATH.PACKS}/${cardPack._id}`)
 
+    }
 
     useEffect(() => {
         dispatch(setCardPackCurrentPageAC(1))
         dispatch(getCardsPacksTC())
     }, [dispatch, isMyTable, debounceLocalPackName, min, max, sortPacks]);
-
-    const handlePopoverClick = (event:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        event.stopPropagation();
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
 
     return (
         <Box className={s.wrapper}>
@@ -151,7 +138,7 @@ export const PackTable = React.memo(() => {
                         <TableHead>
                             <TableRow>
 
-                                <StyledTableCell>
+                                <StyledTableCell align="left" >
                                     <TableSortLabel active={sortPacks === "0name" || sortPacks === "1name"}
                                                     direction={sortPacks === "0name" ? "asc" : "desc"}
                                                     IconComponent={ArrowDropUpIcon}>
@@ -159,7 +146,7 @@ export const PackTable = React.memo(() => {
                                     </TableSortLabel>
                                 </StyledTableCell>
 
-                                <StyledTableCell align="center">
+                                <StyledTableCell align="left" >
                                     <TableSortLabel active={sortPacks === "0cardsCount" || sortPacks === "1cardsCount"}
                                                     direction={sortPacks === "0cardsCount" ? "asc" : "desc"}
                                                     IconComponent={ArrowDropUpIcon}>
@@ -167,7 +154,7 @@ export const PackTable = React.memo(() => {
                                     </TableSortLabel>
                                 </StyledTableCell>
 
-                                <StyledTableCell align="center" className={s.hideForMobile}>
+                                <StyledTableCell align="left" >
                                     <TableSortLabel className={s.headerItem}
                                                     active={sortPacks === "0updated" || sortPacks === "1updated"}
                                                     direction={sortPacks === "0updated" ? "asc" : "desc"}
@@ -176,7 +163,7 @@ export const PackTable = React.memo(() => {
                                     </TableSortLabel>
 
                                 </StyledTableCell>
-                                <StyledTableCell align="center" className={s.hideForMobile}>
+                                <StyledTableCell align="left" className={s.hideForMobile}>
                                     <TableSortLabel active={sortPacks === "0user_name" || sortPacks === "1user_name"}
                                                     direction={sortPacks === "0user_name" ? "asc" : "desc"}
                                                     IconComponent={ArrowDropUpIcon}>
@@ -190,66 +177,35 @@ export const PackTable = React.memo(() => {
                         <TableBody className={s.tableBody}>
                             {
                                 myCardPacks.map((cardPack =>
-                                        <StyledTableRow key={cardPack._id}>
+                                        <StyledTableRow key={cardPack._id} >
 
-                                            <StyledTableCell component="th" scope="row"
-                                                             onClick={(e) => navigateToCardsPage(e, cardPack)}>
-                                                {/*<NavLink to={`${PATH.PACKS}/${cardPack._id}`}>*/}
-                                                {cardPack.name.slice(0, maxCardPackNameLength)}
-                                                {/*</NavLink>*/}
+                                            <StyledTableCell className={s.highlightTableCell} align="left"
+                                                             component="th" scope="row"
+                                                             onClick={(e) => navigateToCardsPage(e, cardPack)}
+                                            >
+                                                <NavLink to={`${PATH.PACKS}/${cardPack._id}`}>
+                                                    {cardPack.name.slice(0, maxCardPackNameLength)}
+                                                </NavLink>
                                             </StyledTableCell>
 
-                                            <StyledTableCell align="left"
-                                                             onClick={(e) => navigateToCardsPage(e, cardPack)}>{cardPack.cardsCount}
+                                            <StyledTableCell className={s.highlightTableCell}
+                                                             align="left" onClick={(e) => navigateToCardsPage(e, cardPack)}
+                                            >{cardPack.cardsCount}
                                             </StyledTableCell>
-                                            <StyledTableCell align="left" className={s.hideForMobile}>
+                                            <StyledTableCell align="left" className={s.hideForMobile} onClick={(e) => navigateToCardsPage(e, cardPack)}>
                                                 {
                                                     moment(cardPack.updated).format("DD.MM.YYYY HH:mm:ss")
                                                 }
                                             </StyledTableCell>
 
-                                            <StyledTableCell align="left"
-                                                             onClick={(e) => navigateToCardsPage(e, cardPack)}
+                                            <StyledTableCell align="left" onClick={(e) => navigateToCardsPage(e, cardPack)}
                                                              className={s.hideForMobile}>{cardPack.user_name.slice(0, 45)}
                                             </StyledTableCell>
-                                            <StyledTableCell align="right">
-                                                <Box className={s.mobileButtonGroup}>
-                                                <IconButton onClick={handlePopoverClick}>
-                                                    <InfoOutlinedIcon className={s.iconInfo}/>
-                                                </IconButton>
-                                                <IconButton onClick={() => handleOpenLearn(cardPack)} >
-                                                    <SchoolOutlinedIcon className={s.iconLearn}/>
-                                                </IconButton>
-                                                {cardPack.user_id === myId && (
-                                                    <>
-                                                        <IconButton onClick={() => handleOpenEdit(cardPack)} >
-                                                            <EditOutlinedIcon className={s.iconEdit}/>
-                                                        </IconButton>
-                                                        <IconButton onClick={() => handleOpenDelete(cardPack)} >
-                                                            <DeleteSweepOutlinedIcon className={s.iconDelete}/>
-                                                        </IconButton>
-                                                    </>
-                                                )}
-                                                <Popover
-                                                    open={open}
-                                                    anchorEl={anchorEl}
-                                                    onClose={handlePopoverClose}
-                                                    anchorOrigin={{
-                                                        vertical: 'bottom',
-                                                        horizontal: 'left',
-                                                    }}
-                                                    className={s.popover}
-                                                >
-                                                    <div className={s.popoverDiv}>
-                                                        <span>hello, this is popover</span>
-                                                    </div>
-                                                </Popover>
-                                            </Box>
+                                            <StyledTableCell align="left">
                                                 <Box className={s.buttonGroup}>
                                                     {cardPack.user_id === myId && (
                                                         <>
-                                                            <button name="delete"
-                                                                    onClick={() => handleOpenDelete(cardPack)}
+                                                            <button name="delete" onClick={() => handleOpenDelete(cardPack)}
                                                                     className={s.delete}>Delete
                                                             </button>
                                                             <button name="edit" onClick={() => handleOpenEdit(cardPack)}
@@ -264,7 +220,9 @@ export const PackTable = React.memo(() => {
                                             </StyledTableCell>
                                         </StyledTableRow>
                                 ))
+
                             }
+
 
                         </TableBody>
                     </Table>
@@ -279,8 +237,8 @@ export const PackTable = React.memo(() => {
                 )
             }
             {totalCardPacksPageCount > 1 &&
-                <Pagination onChange={handleChangePagination} count={totalCardPacksPageCount} page={currentPage}
-                            shape="rounded"/>}
+            <Pagination onChange={handleChangePagination} count={totalCardPacksPageCount} page={currentPage}
+                        shape="rounded"/>}
 
             <DeleteModalContainer styles={modalStyle} pack={rowToDelete} closeModalCallback={setRowToDelete}/>
             <EditAddModalContainer styles={modalStyle} pack={rowToUpdate} closeModalCallback={setRowToUpdate}/>
